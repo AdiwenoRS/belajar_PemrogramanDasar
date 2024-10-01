7f025c411a23c90a8b45585f285f91c8a61772d3
AdiwenoRS <adiwenors@gmail.com>
2024-10-01 23:04:57 +0700

    changing formula of persegi panjang

--- Tugas/KelilingPersegi.py
+++ Tugas/KelilingPersegi.py
@@ -1,8 +1,6 @@
 panjang = int(input("Masukkan panjang persegi: "))
 lebar = int(input("Masukkan lebar persegi: "))
-tinggi = int(input("Masukkan tinggi persegi: "))
-
-keliling = panjang * lebar * tinggi
+keliling = 2 * (panjang + lebar)
 
 print("Keliling permukaan persegi tersebut adalah:", keliling, "CM")
 
