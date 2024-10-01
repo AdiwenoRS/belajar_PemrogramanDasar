de5f850a2235c9105536e9cf1a0324796cb3a0ef
AdiwenoRS <adiwenors@gmail.com>
2024-10-02 02:07:23 +0700

    cleaning code

--- Tugas/hitungUmurV2.py
+++ Tugas/hitungUmurV2.py
@@ -1,14 +1,13 @@
 from datetime import date
-today = date.today()
 
+today = date.today()
 tahunLahir = int(input("Masukkan tahun lahir anda: "))
 bulanLahir = int(input("Masukkan bulan lahir anda: "))
 tanggalLahir = int(input("Masukkan tanggal lahir anda: "))
-
 date = [int(today.strftime("%Y")), int(today.strftime("%m")), int(today.strftime("%d"))]
 yearCalc = (date[0] - tahunLahir)
 
 if (bulanLahir >= date[1] and tanggalLahir >= date[2]):
-    yearCalc += 1
+    yearCalc -= 1
 
 print("Umur anda adalah", yearCalc, "Tahun")
