cef66f50887076b2756db95f08bdb63e1c1e11fa
AdiwenoRS <adiwenors@gmail.com>
2024-10-01 23:24:39 +0700

    inputOutput

--- Tugas/KelilingPersegi.py
+++ Tugas/KelilingPersegi.py
@@ -1,6 +1,12 @@
-panjang = int(input("Masukkan panjang persegi: "))
-lebar = int(input("Masukkan lebar persegi: "))
-keliling = 2 * (panjang + lebar)
+panjang = int(input("Masukkan panjang persegi: ")) #input variabel panjang
+lebar = int(input("Masukkan lebar persegi: ")) #input variabel lebar
+keliling = 2 * (panjang + lebar) #menghitung keliling
 
-print("Keliling permukaan persegi tersebut adalah:", keliling, "CM")
+print("Keliling permukaan persegi tersebut adalah:", keliling, "CM") #menampilkan keliling
+
+'''
+Output: 
+Masukkan panjang persegi: 10
+Masukkan lebar persegi: 10
+Keliling permukaan persegi tersebut adalah: 40 CM'''
 
---
+++ Tugas/inputOutput.py
@@ -0,0 +1,8 @@
+print(f"{5*"+="} Data Diri Mahasiswa {5*"+="}")
+nim = input("Masukkan NIM: ")
+nama = input("Masukkan Nama: ") 
+jurusan = input("Masukkan Jurusan: ")
+alamat = input("Masukkan Alamat: ")
+
+print("\nHasil Cetak data diatas adalah")
+print(f"NIM: {int(nim)} \nNama: {nama} \nJurusan: {jurusan} \nAlamat: {alamat}")
