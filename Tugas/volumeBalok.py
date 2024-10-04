5d0ea88b139f21eafc7c3f2fd0619ff249e7926d
AdiwenoRS <adiwenors@gmail.com>
2024-10-04 14:06:30 +0700

    change from rectangle to rectangular

---
+++ Tugas/volumeBalok.py
@@ -0,0 +1,6 @@
+panjang = int(input("Masukkan panjang balok: ")) #input variabel panjang
+lebar = int(input("Masukkan lebar balok: ")) #input variabel lebar
+tinggi = int(input("Masukkan tinggi balok: ")) #input variabel tinggi
+volumeBalok = panjang*lebar*tinggi #kalkulasi
+
+print("Keliling permukaan persegi tersebut adalah:", volumeBalok, "CM") #menampilkan volume
